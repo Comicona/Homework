904ea58ba4b53c401c826c788df0b6a061b52014
Comicona <ulyana.gorbatikova@gmail.com>
2020-12-18 22:25:49 +0300

    ibm calculator was updated

--- ibm_calculator.py
+++ ibm_calculator.py
@@ -3,5 +3,9 @@ m = input('Please, enter your mass in kilograms:')
 
 I = float(m) / float(h)**2
 
-print('Your IBM is ' + str(int(I))
-     + '\n20' + '='*(int(I)-20) + '|' + '='*(50-int(I)) + '50')
+if (int(I)>=15 and int(I)<=50):
+    print('Your IBM is ' + str(int(I))
+     + '\n15' + '='*(int(I)-15) + '|' + '='*(50-int(I)) + '50')
+else:
+    print('Your IBM is ' + str(int(I))
+    +'\nSorry, your IBM seems to be incorrect or out of range')
